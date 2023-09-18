649ea1cac6a18a9e0ef248eb88ddde5473b61e25
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2023-09-18 11:01:37 +0200

    Add files via upload

---
+++ Ultraspikebalatonosja/Balcsika.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ultraspikebalatonosja
+{
+    class Balcsika
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public string VersenyzoKategoria { get; set; }
+        public int IdoEredmeny { get; set; }
+        public int TeljesTav { get; set; }
+
+        public Balcsika(string r)
+        {
+        string[] v = r.Split(";");
+            Nev = v[0];
+            Rajtszam = v[1];
+            VersenyzoKategoria = v[2];
+            IdoEredmeny = v[3];
+            TeljesTav = v[4];
+        }
+    }
+}
+
---
+++ Ultraspikebalatonosja/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ultraspikebalatonosja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var versenyzok = List<Balcsika>();
+            using var sr = StreamReader(
+                "@//..//..//..ub2017"
+                )
+        }
+    }
+}
